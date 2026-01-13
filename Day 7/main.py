3e2bc5e94d17716b79505cab9921b4221e696d42
sulafasirin <sulafasirin@gmail.com>
2026-01-13 16:33:32 +0530

    commit

---
+++ Day 7/main.py
@@ -0,0 +1,8 @@
+import math_utils
+res1=math_utils.add(10,5)
+res2=math_utils.subtract(12,10)
+res3=math_utils.multiply(3,4)
+
+print("Addition is",res1)
+print("subtraction is",res2)
+print("multiplication is",res3)
